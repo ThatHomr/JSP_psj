f8f2873a2dca468813ac66f771cfd74216fa12a9
Park Sungjun <34852381a@gmail.com>
2022-11-15 18:30:59 +0900

    22-11-15 첫번째 commit

---
+++ workspace/day05/src/com/online/servlet/Calc.java
@@ -0,0 +1,21 @@
+package com.online.servlet;
+
+public class Calc {
+
+	int num1;
+	int num2;
+	
+	public Calc() {;}
+	
+	public Calc(int num1, int num2) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	//덧셈
+	//뺄셈
+	//곱셈
+	//나눗셈
+	
+}
